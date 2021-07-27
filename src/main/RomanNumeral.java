91ce57315c0de7b84eb5fe9cd7c28eec2268ca49
ZaicevSergey <77642306+ZaicevSergey@users.noreply.github.com>
2021-07-27 14:16:49 +0300

    Fixed errors when calculating expressions IX * IX and IX - V

--- src/main/RomanNumeral.java
+++ src/main/RomanNumeral.java
@@ -3,8 +3,13 @@ package main;
 enum RomanNumeral {
     C(100),
     XC(90),
+    LXXX(80),
+    LXX(70),
+    LX(60),
     L(50),
     XL(40),
+    XXX(30),
+    XX(20),
     X(10),
     IX(9),
     V(5),
